c31668c9b5e3564f4c224598c6d3e931baa03430
Najja O'Connor <najjaoconnor@icloud.com>
2020-04-13 13:59:33 -0400

    fixed pounds to grams conversion

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById('lbsInput').addEventListener('input',
         document.getElementById('output').style.visibility = 'visible';
         let pounds = e.target.value;
         console.log(pounds)
-        document.getElementById('gramsOutput').innerHTML = pounds / 0022046;
+        document.getElementById('gramsOutput').innerHTML = pounds / 0.0022046;
         document.getElementById('kgOutput').innerHTML = pounds / 2.2046;
         document.getElementById('ozOutput').innerHTML = pounds * 16;
 
