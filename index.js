248ff2a4fed1a256d98866a965ff11e7673e2c40
Najja O'Connor <najjaoconnor@icloud.com>
2020-04-13 13:35:54 -0400

    made external js file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 document.getElementById('output').style.visibility = 'hidden';
-document.getElementById('lbsInput').addEventListener('input', 
-function (e){
-document.getElementById('output').style.visibility = 'visible';
-let pounds = e.target.value;
-console.log(pounds)
-document.getElementById('gramsOutput').innerHTML = pounds/0022046;
-document.getElementById('kgOutput').innerHTML = pounds/2.2046;
-document.getElementById('ozOutput').innerHTML = pounds * 16;
+document.getElementById('lbsInput').addEventListener('input',
+    function (e) {
+        document.getElementById('output').style.visibility = 'visible';
+        let pounds = e.target.value;
+        console.log(pounds)
+        document.getElementById('gramsOutput').innerHTML = pounds / 0022046;
+        document.getElementById('kgOutput').innerHTML = pounds / 2.2046;
+        document.getElementById('ozOutput').innerHTML = pounds * 16;
 
 
-})
+    })
